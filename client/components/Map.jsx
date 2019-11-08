85a638bffb15d635b9532ac8cfb0a41bfd2e47dd
Ruth Hill <ruthhill.e@gmail.com>
2019-11-08 14:11:05 -0500

    began adding mouse position functionality, removed firebase

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -7,11 +7,30 @@ class Map extends Component {
   componentDidMount() {
     MapboxGl.accessToken = "pk.eyJ1IjoicnV0aHRvd24iLCJhIjoiY2sybDBzd2VvMDI2cjNvcG43YzdxZHptcyJ9.39XFWCL8XvT7UqVK7M8BLg";
 
-    new MapboxGl.Map({
+    let click = document.getElementById('click');
+    let mousemove = document.getElementById('mousemove');
+
+    // const map = L.mapbox.map('map')
+    //     .setView([0, 0], 3)
+    //     .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+    //
+    // map.on('mousemove click', function(e) {
+    //     window[e.type].innerHTML = e.containerPoint.toString() + ', ' + e.latlng.toString();
+    //     console.log("Mouse Position: ", e.containerPoint.toString() + ', ' + e.latlng.toString())
+    // });
+
+    let mapInstance = new MapboxGl.Map({
       container: "map",
       center: [4.626541, 28.582692],
       zoom: 1, // starting zoom
       style: "mapbox://styles/ruthtown/cjy1zfey01ai51cp31xzhmwnw"
+    });
+
+    mapInstance.on('click', function(e) {
+      console.log(e);
+      console.log(e.lngLat.lat, " ", e.lngLat.lng)
+        // window[e.type].innerHTML = e.containerPoint.toString() + ', ' + e.latlng.toString();
+        // console.log("Mouse Position: ", e.containerPoint.toString() + ', ' + e.latlng.toString());
     });
   }
 
