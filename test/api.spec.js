cc9ca874e57b9034d526a8fba69930e73da9908c
Ruth H <ruthhill.e@gmail.com>
2020-02-03 17:53:46 -0500

    Update api.spec.js

--- test/api.spec.js
+++ test/api.spec.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 
 const app = require('../app');
 // const agent = request.agent(app);
+const token = require('../secrets');
 
 describe('GET /', () => {
   it('On load, app responds with status code 200', (done) => {
@@ -16,7 +17,6 @@ describe('GET /', () => {
 describe('GET /api/marker/:lat/:lng/:token', () => {
   const lat = '48.869332075570895';
   const lng = '2.3439430708154134';
-  const token = 'pk.eyJ1IjoicnV0aHRvd24iLCJhIjoiY2sybDBzd2VvMDI2cjNvcG43YzdxZHptcyJ9.39XFWCL8XvT7UqVK7M8BLg';
 
   it('On add marker, object is returned containing the corresponding place name', (done) => {
     request(app)
@@ -33,28 +33,6 @@ describe('GET /api/marker/:lat/:lng/:token', () => {
 // Front end add marker tests:
 // - sidebar should be visible
 // - sidebar should include a list item with the corresponding place name
-
-
-/*
-describe('/api/contracts/', () => {
-
-  beforeEach(() => {
-      return Contract.create({contractAddress: 123456789})
-  })
-
-  it('PUT /api/contracts should change from status "Created" to status "Pending"', () => {
-
-    return request(app)
-      .put('/api/contracts/1')
-      .send({status: 'Pending'})
-      .expect(200)
-      .then(res => {
-        expect(res.body).to.be.an('object')
-        expect(res.body.status).to.equal('Pending')
-      })
-  })
-})
-*/
 
 // Functionality yet to be added (and subsequently tested):
 // - Login / Logout
