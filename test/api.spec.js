860cbaff4df194fd55b2cf8f010bb611088fb049
Ruth Hill <ruthhill.e@gmail.com>
2020-02-03 17:51:01 -0500

    Added 1 test, updated Mapbox API key and moved to secrets.js

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,23 +2,60 @@ const { expect } = require('chai');
 const request = require('supertest');
 
 const app = require('../app');
-
-// If cookies are needed:
 // const agent = request.agent(app);
 
 describe('GET /', () => {
-  it('responds with status code 200', (done) => {
+  it('On load, app responds with status code 200', (done) => {
     request(app)
       .get('/')
-      .expect(200, done);
+      .expect(200)
+      .end(done);
   });
 });
 
-describe('GET /api/users', () => {
-  it('responds with json', (done) => {
+describe('GET /api/marker/:lat/:lng/:token', () => {
+  const lat = '48.869332075570895';
+  const lng = '2.3439430708154134';
+  const token = 'pk.eyJ1IjoicnV0aHRvd24iLCJhIjoiY2sybDBzd2VvMDI2cjNvcG43YzdxZHptcyJ9.39XFWCL8XvT7UqVK7M8BLg';
+
+  it('On add marker, object is returned containing the corresponding place name', (done) => {
     request(app)
-      .get('/api/users')
+      .get(`/api/marker/${lat}/${lng}/${token}`)
       .expect('Content-Type', /json/)
-      .expect(200, done);
+      .expect(200)
+      .expect((res) => {
+        res.body.place_name = 'Paris, France';
+      })
+      .end(done);
   });
 });
+
+// Front end add marker tests:
+// - sidebar should be visible
+// - sidebar should include a list item with the corresponding place name
+
+
+/*
+describe('/api/contracts/', () => {
+
+  beforeEach(() => {
+      return Contract.create({contractAddress: 123456789})
+  })
+
+  it('PUT /api/contracts should change from status "Created" to status "Pending"', () => {
+
+    return request(app)
+      .put('/api/contracts/1')
+      .send({status: 'Pending'})
+      .expect(200)
+      .then(res => {
+        expect(res.body).to.be.an('object')
+        expect(res.body.status).to.equal('Pending')
+      })
+  })
+})
+*/
+
+// Functionality yet to be added (and subsequently tested):
+// - Login / Logout
+// - Text search
