90cd44ccfd27a64c5bd2de8b554e4d102de353f8
Ruth Hill <ruthhill.e@gmail.com>
2020-03-02 11:44:04 -0500

    Added 1 new test case for MarkerLists, minor updates to other tests

--- test/user.spec.js
+++ test/user.spec.js
@@ -3,23 +3,17 @@ const request = require('supertest');
 const app = require('../app');
 const models = require('../db/models');
 
-const { User } = models;
+const { User, MarkerList } = models;
 
-describe('User routes', () => {
+describe('USER', () => {
   beforeEach(() => models.db.sync({ force: true }));
 
-  describe('/api/users/', () => {
+  describe('/api/users', () => {
     beforeEach(() => User.create({
       username: 'ChurchPeanut',
       email: 'churchp@catmail.com',
       password: '123',
     }));
-
-    // MarkerList.create({
-    //   title: 'A Cat\'s Journey',
-    //   notes: 'The most illustrious cat details her intrepid voyage',
-    //   ownerId: createdUser.id,
-    // })
 
     it('GET /api/users', () => {
       return request(app)
@@ -27,7 +21,31 @@ describe('User routes', () => {
         .expect(200)
         .then((res) => {
           expect(res.body).to.be.an('array');
-          expect(res.body[0].email).to.be.equal('churchp@catmail.com');
+          expect(res.body[0].email).to.equal('churchp@catmail.com');
+        });
+    });
+  });
+
+  describe('/api/lists/:userId', () => {
+    beforeEach(() => User.create({
+      id: 1,
+      username: 'ChurchPeanut',
+      email: 'churchp@catmail.com',
+      password: '123',
+    })
+      .then((createdUser) => MarkerList.create({
+        title: 'A Cat\'s Journey',
+        notes: 'The Most Illustrious Cat details her intrepid voyage',
+        ownerId: createdUser.id,
+      })));
+
+    it('GET /api/lists/:userId', () => {
+      return request(app)
+        .get('/api/lists/1')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).to.be.an('array');
+          expect(res.body[0].title).to.equal('A Cat\'s Journey');
         });
     });
   });
