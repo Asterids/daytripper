d2920c5f7f27b44feb1e4cb93cdb18870174883f
Ruth Hill <ruthhill.e@gmail.com>
2020-07-31 13:55:34 -0400

    Fixed login & signup button copy

--- client/components/IntroCard.jsx
+++ client/components/IntroCard.jsx
@@ -15,10 +15,10 @@ const IntroCard = (props) => {
         <b>Welcome to DayTripper!</b>
       </h6>
       <h6>
-        → Begin by adding markers to the map. You'll need to login if you want to start saving your lists.
+        → Begin by adding markers to the map. You'll need to log in if you want to start saving your lists.
       </h6>
       <h6>
-        → You can <button type="button" className="greenLink" onClick={handleOpenLogin}>login as a guest</button> if you just want to see how things work!
+        → You can <button type="button" className="greenLink" onClick={handleOpenLogin}>log in as a guest</button> if you just want to see how things work!
       </h6>
       <button type="button" className="close secondaryButton" onClick={toggleIntroCard}>x</button>
     </div>
